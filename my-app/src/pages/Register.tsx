3ebe982f88cb88e892c9d53f3130b9c839f37e68
VTellesRg <vini.m.telles@gmail.com>
2023-03-31 00:05:27 -0300

    html changes at Register.tsx

--- my-app/src/pages/Register.tsx
+++ my-app/src/pages/Register.tsx
@@ -6,24 +6,23 @@ import Button from "../components/Button";
 export default function Register() {
   return (
     <div>
-      <div className="div">
-        <h1 className="title">Olá,</h1>
-        <p>Por favor, registre-se para continuar</p>
+      <div className="header">
+        <h1 className="header__title">Olá,</h1>
+        <p className="header__p">Por favor, registre-se para continuar</p>
       </div>
       <form className="form" action="#">
-        <h2>Registro</h2>
-        <CustomInput onFocus={() => { }} placeholder="Nome" />
-        <CustomInput onFocus={() => { }} placeholder="Usuário" />
-        <CustomInput onFocus={() => { }} placeholder="Nascimento" />
-        <CustomInput onFocus={() => { }} placeholder="Email" />
-        <CustomInput onFocus={() => { }} placeholder="Senha" />
-        <CustomInput onFocus={() => { }} placeholder="Confirmar Senha" />
-
+        <h2 className="form_h2">Registro</h2>
+        <CustomInput placeholder="Nome" />
+        <CustomInput placeholder="Usuário" />
+        <CustomInput placeholder="Nascimento" />
+        <CustomInput placeholder="Email" />
+        <CustomInput placeholder="Senha" />
+        <CustomInput placeholder="Confirmar Senha" />
         <Button />
       </form>
       <div className="footer">
         <p className="text__footer">
-          Já possui uma conta? <Link to="/login">Faça Login</Link>
+          Já possui uma conta? <Link to="/" className="text__footer">Faça Login</Link>
         </p>
       </div>
     </div>
