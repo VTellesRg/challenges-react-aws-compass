18327622eee860a1aa34b397ac67d6a0cf0c9ab9
VTellesRg <vini.m.telles@gmail.com>
2023-05-22 16:37:32 -0300

    refactor: friends to render from component folder

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -4,8 +4,7 @@ import React, { useEffect } from "react";
 import { UserType, PostType } from "../../types/type";
 import { useNavigate, useLocation } from "react-router-dom";
 import {
-    ChevronUpIcon,
-    ChevronDownIcon,
+   
     HomeIcon,
     CameraIcon,
     PhotoIcon,
@@ -278,31 +277,6 @@ export default function Home() {
                         }
                     </div>
 
-                    {/* <div className="topics">
-                        <div className={arrow ? "trend" : "trend-colapsed"}>
-                            <div className="trend-header">
-                                <h1 className="trend-header-title">Meus Amigos</h1>
-                                {arrow ?
-                                    <ChevronDownIcon className="trend-arrow" onClick={() => setArrow(false)} /> :
-                                    <ChevronUpIcon className="trend-arrow" onClick={() => setArrow(true)} />}
-                            </div>
-                            {arrow &&
-                                <div className="friends-list">
-                                    {users && users.map((user: UserType, key) => {
-                                        return (
-                                            <div className="friend" key={key}>
-                                                <img className="friend-image" src={user.profile_photo} />
-                                                <h1 className="friend-name">{user.name}</h1>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            }
-                        </div>
-                        <div className="trend" />
-                        <div className="trend" />
-
-                    </div> */}
                     <Friends arrow={arrow} setArrow={setArrow} users={users}
                     />
 
